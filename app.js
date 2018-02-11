1643f766557b68d5480d0534338be5c93e4a66a3
aimishan <aimishan@163.com>
2018-02-11 23:17:25 +0800

    本次更新颇多： 1. 引进了全新的框架（Vue.js） 2. 在全新框架的基础上采用了更为自然的动画效果 3. 更换了全新的字体（DroidSansMono） 4. 结构化整个程序，使得Javascript代码、CSS代码和HTML代码有效分离 5. 缩短了slidebar的宽度 6. 进一步精简了代码的结构层次，使之更为清晰 7. 这里提到，为了实现更为自然的动画效果，我引进了animate.css这一动画库 8. 完全重写了discovery部分，现在切换左右图片的控件转移到了bottombar上（但只有在discovery组件被激活时才会显示） 9. 这里提到，为了使图片的播放（滑动）更为自然，我使用了v-for语法，并吸取了网络上分享而来的经验 10. 遗留的问题：       暂未实现essays的部分       暂时没有完全处理切换图片操作显示逻辑的问题       暂时没有为切换图片的控件添加过度动画 11. 更多更新不再详叙

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-
+main = new Vue({
+    el: "#main-container",
+    data: {
+        slidebarShow: false,
+        lettersShow: false,
+        discoveryShow: false,
+        aboutShow: false,
+        controlbarShow: false,
+        currentImg: 0,
+        image: ["1.png", "2.png", "3.png"]
+    },
+    methods: {
+        toggleSlidebar: function() {
+            this.slidebarShow = !this.slidebarShow
+        },
+        toggleLetters: function() {
+            this.lettersShow = !this.lettersShow
+        },
+        toggleDiscovery: function() {
+            this.discoveryShow = !this.discoveryShow
+            this.controlbarShow = !this.controlbarShow
+        },
+        toggleAbout: function() {
+            this.aboutShow = !this.aboutShow
+        },
+        switchPhotoLeft: function() {
+            if(this.currentImg < 2)
+                this.currentImg += 1
+        },
+        switchPhotoRight: function() {
+            if(this.currentImg > 0)
+                this.currentImg -= 1
+        }
+    }
+})
