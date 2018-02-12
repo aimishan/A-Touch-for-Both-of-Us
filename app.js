b02179e496120c4b10a85a4a8460077d08697ac1
aimishan <aimishan@163.com>
2018-02-12 13:48:11 +0800

    这一次的更新包括： 1. 完成了应用程序的最后一部分（essays.-container） 2. 实现了各组件的避免冲突的问题 3. 解决了某些组件覆盖下方组件的问题（通过jQuery动态修改组件z-index属性） 4. 通过修改动画效果，修复了照片切换逻辑不符合直觉的问题 5. 这里提到，实现避免组件冲突的方法与上一版程序有所不同，这一次采用了在Vue父组件中标记当前组件（currentWidget），而后通过切换当前组件标识的方式来控制组件Show属性的true与false 6. 接第5点，在解决组件冲突过程中，添加了监视（slidebarShow和lettersShow）

--- app.js
+++ app.js
@@ -5,23 +5,73 @@ main = new Vue({
         lettersShow: false,
         discoveryShow: false,
         aboutShow: false,
-        controlbarShow: false,
+        noneWidget: true,
         currentImg: 0,
-        image: ["1.png", "2.png", "3.png"]
+        image: ["1.png", "2.png", "3.png"],
+        currentEssay: 0,
+        currentWidget: "noneWidget"
+    },
+    watch: {
+        slidebarShow: function() {
+            if(this.slidebarShow == false)
+                this.currentEssay = 0;
+        },
+        lettersShow: function() {
+            if(this.lettersShow == true)
+                $("#essays-container").css("z-index", 5)
+            else $("#essays-container").css("z-index", 7)
+        }
+    },
+    computed: {
+        controlbarShow: function() {
+            return this.discoveryShow
+        }
     },
     methods: {
         toggleSlidebar: function() {
-            this.slidebarShow = !this.slidebarShow
+            if(this.currentWidget != "slidebarShow") {
+                this[this.currentWidget] = false;
+                this.currentWidget = "slidebarShow";
+                this.slidebarShow = true;
+            }else{
+                this.slidebarShow = false;
+                this.noneWidget = true;
+                this.currentWidget="noneWidget"
+            }
+            if(this.currentEssay != 0)  this.currentEssay = 0;
         },
         toggleLetters: function() {
-            this.lettersShow = !this.lettersShow
+            if(this.currentWidget != "lettersShow") {
+                this[this.currentWidget] = false;
+                this.currentWidget = "lettersShow";
+                this.lettersShow = true;
+            }else{
+                this.lettersShow = false;
+                this.noneWidget = true;
+                this.currentWidget="noneWidget"
+            }
         },
         toggleDiscovery: function() {
-            this.discoveryShow = !this.discoveryShow
-            this.controlbarShow = !this.controlbarShow
+            if(this.currentWidget != "discoveryShow") {
+                this[this.currentWidget] = false;
+                this.currentWidget = "discoveryShow";
+                this.discoveryShow = true;
+            }else{
+                this.discoveryShow = false;
+                this.noneWidget = true;
+                this.currentWidget="noneWidget"
+            }
         },
         toggleAbout: function() {
-            this.aboutShow = !this.aboutShow
+            if(this.currentWidget != "aboutShow") {
+                this[this.currentWidget] = false;
+                this.currentWidget = "aboutShow";
+                this.aboutShow = true;
+            }else{
+                this.aboutShow = false;
+                this.noneWidget = true;
+                this.currentWidget="noneWidget"
+            }
         },
         switchPhotoLeft: function() {
             if(this.currentImg < 2)
